52a7768146a34f6c8f75b83805267eef9df3118f
Mahidun Nobi <mdmahidunnobi@gmail.com>
2024-08-10 19:49:19 +0600

    Add: Sponsor section in home page

---
+++ src/Components/Home/Sponsors/Sponsors.tsx
@@ -0,0 +1,29 @@
+import UnipolIcon from "../../../assets/Logos/Unipol-Code.svg";
+import PropertyIcon from "../../../assets/Logos/Property-Ombudsman.png";
+import TDSIcon from "../../../assets/Logos/Tds-Combined.svg";
+import NagotiatorIcon from "../../../assets/Logos/The-Negotiator-Awards.svg";
+import UnihomesIcon from "../../../assets/Logos/unihomes.svg";
+
+const Sponsors = () => {
+  return (
+    <div className="container mx-auto px-3 my-12">
+      <p className="text-sm text-center hidden md:block">
+        HAUS IS AN AWARD WINNING AND FULLY ACCREDITED PROPERTY SALES AND LETTING
+        AGENCY IN YORKSHIRE
+      </p>
+      <div className="flex items-center justify-center gap-2 md:gap-6">
+        <img src={UnipolIcon} alt="" className="w-16 md:w-36" />
+        <img src={PropertyIcon} alt="" className="w-16 md:w-36" />
+        <img src={TDSIcon} alt="" className="w-16 md:w-36" />
+        <img src={NagotiatorIcon} alt="" className="w-16 md:w-36" />
+        <img src={UnihomesIcon} alt="" className="w-16 md:w-36" />
+      </div>
+      <p className="text-sm text-center md:hidden mt-3">
+        HAUS IS AN AWARD WINNING AND FULLY ACCREDITED PROPERTY SALES AND LETTING
+        AGENCY IN YORKSHIRE
+      </p>
+    </div>
+  );
+};
+
+export default Sponsors;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../../Components/Home/Hero/Hero";
+import Sponsors from "../../Components/Home/Sponsors/Sponsors";
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <Sponsors />
     </main>
   );
 };
