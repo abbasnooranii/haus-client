89199cd9e5451f9effc1e131581604c0ca5a211e
Mahidun Nobi <mdmahidunnobi@gmail.com>
2024-08-12 18:04:24 +0600

    Add: Maps & Street Section - Haus details page

--- src/Components/HauseDetails/HausDetails/HausDetails.tsx
+++ src/Components/HauseDetails/HausDetails/HausDetails.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Menu from "./Menu";
 import Description from "./Description/Description";
+import MapStreet from "./MapStreet/MapStreet";
 
 const HausDetails = () => {
   const [selectedSpec, setSelectedSpec] = useState<string>("map-street");
@@ -8,6 +9,7 @@ const HausDetails = () => {
     <div className="container mx-auto px-5 my-12">
       <Menu selectedSpec={selectedSpec} setSelectedSpec={setSelectedSpec} />
       {selectedSpec === "description" && <Description />}
+      {selectedSpec === "map-street" && <MapStreet />}
     </div>
   );
 };
