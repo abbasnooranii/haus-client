7c0ff1675a6cfd4ee9e9724f8ba8093c4de864f9
Mahidun Nobi <mdmahidunnobi@gmail.com>
2024-08-12 18:43:06 +0600

    Add: EPC section - Haus Details page

--- src/Components/HauseDetails/HausDetails/HausDetails.tsx
+++ src/Components/HauseDetails/HausDetails/HausDetails.tsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import Menu from "./Menu";
 import Description from "./Description/Description";
 import MapStreet from "./MapStreet/MapStreet";
+import Epc from "./Epc/Epc";
 
 const HausDetails = () => {
-  const [selectedSpec, setSelectedSpec] = useState<string>("map-street");
+  const [selectedSpec, setSelectedSpec] = useState<string>("epc");
   return (
     <div className="container mx-auto px-5 my-12">
       <Menu selectedSpec={selectedSpec} setSelectedSpec={setSelectedSpec} />
       {selectedSpec === "description" && <Description />}
       {selectedSpec === "map-street" && <MapStreet />}
+      {selectedSpec === "epc" && <Epc />}
     </div>
   );
 };
